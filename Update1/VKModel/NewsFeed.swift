51e7efa38bb2201def6512d7ec817d1a619f4f66
DSAlchemist <ccdefs@gmail.com>
2021-03-05 18:37:35 +0300

    Update1

---
+++ Update1/VKModel/NewsFeed.swift
@@ -0,0 +1,24 @@
+//
+//  NewsFeed.swift
+//  MProject
+//
+//  Created by Danil G. on 05.03.2021.
+//
+
+import Foundation
+//import RealmSwift
+//
+//class NewsModel: Object, Codable {
+//    @objc dynamic var postID: Int = 0
+//    @objc dynamic var text: String = ""
+//
+//    convenience init(postID: Int, text: String) {
+//        self.init()
+//        self.postID = postID
+//        self.text = text
+//    }
+//
+//    enum CodingKeys: String, CodingKey {
+//        case postID = "post_id", text
+//    }
+//}
