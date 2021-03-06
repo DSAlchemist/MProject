d76046c8ad9acad13727a62776200276856e0a84
DSAlchemist <ccdefs@gmail.com>
2021-03-06 21:47:09 +0300

    Update2.01

---
+++ Update2.01/Groups/MyGroupsTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  MyGroupsTableViewCell.swift
+//  MProject
+//
+//  Created by Danil G. on 06.03.2021.
+//
+
+import UIKit
+
+class MyGroupsTableViewCell: UITableViewCell {
+
+//    @IBOutlet weak var groupImage: UIImageView!
+//    @IBOutlet weak var groupName: UILabel!
+//    @IBOutlet weak var groupImageView: SetImageShadow!
+  
+    
+//    func configure(with group: Community) {
+//        self.groupName.text = group.groupName
+//        let url = URL(string: group.groupImage)
+//        self.groupImage.kf.setImage(with: url)
+//    }
+}
