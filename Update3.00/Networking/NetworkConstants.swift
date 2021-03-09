3d0f72957366ad4f37568b983a83042f066d892f
DSAlchemist <ccdefs@gmail.com>
2021-03-09 19:19:02 +0300

    Update3.00

---
+++ Update3.00/Networking/NetworkConstants.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkConstants.swift
+//  MProject
+//
+//  Created by Danil G. on 09.03.2021.
+//
+
+import Foundation
+
+struct NetworkConstants {
+    let clientID = "7776061"
+    let display = "mobile"
+    let redirectURI = "https://oauth.vk.com/blank.html"
+    let scope = "262150"
+    let responseType = "token"
+    let APIversion = "5.130"
+}
