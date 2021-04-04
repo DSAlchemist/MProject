68a354fcd63eedbbe984c48edec81953080a592c
DSAlchemist <ccdefs@gmail.com>
2021-04-04 21:15:32 +0300

    Update 4.1.2

--- News/NewsFeedViewController.swift
+++ News/NewsFeedViewController.swift
@@ -7,23 +7,76 @@
 
 import UIKit
 
-class NewsFeedViewController: UIViewController {
+class NewsFeedTableViewController: UITableViewController {
 
+//    let networkManager = NetworkManager.shared
+//    private var newsPosts = [NewsPost]()
+//    private var users = [Int:User]()
+//    private var groups = [Int:Group]()
+//
 //    override func viewDidLoad() {
 //        super.viewDidLoad()
-//        setup()
-//
-//        table.register(UINib(nibName: "NewsFeedCell", bundle: nil), forCellReuseIdentifier: NewsFeedCell.reuseId)
-//
-//        // Do any additional setup after loading the view.
+//        networkManager.loadNewsPost(token: Session.shared.token) { [weak self] news, users, groups in
+//            DispatchQueue.main.async {
+//                self?.newsPosts = news
+//                self?.users = users
+//                self?.groups = groups
+//                self?.tableView.reloadData()
+//            }
+//        }
+//        tableView.register(UINib(nibName: Constants.newsCellIdentifier, bundle: nil), forCellReuseIdentifier: Constants.newsCellIdentifier)
 //    }
 //
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: NewsFeedCell.reuseId, for: IndexPath)
-//            as! NewsFeedCell
-//        return cell
+// 
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return newsPosts.count
 //    }
-
-    // MARK: - Navigation
-
+//
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        if let cell = tableView.dequeueReusableCell(withIdentifier: Constants.newsCellIdentifier,
+//                                                    for: indexPath) as? NewsCell {
+//            let news = newsPosts[indexPath.row]
+//
+//            parseTableData(news: news) { (newsPostViewModel) in
+//                
+//                
+//                DispatchQueue.main.async {
+//                    cell.setup(newsPostViewModel: newsPostViewModel)
+//                }
+//            }
+//            
+//            return cell
+//        }
+//        return UITableViewCell()
+//    }
+//    
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        tableView.deselectRow(at: indexPath, animated: true)
+//        tableView.beginUpdates()
+//        
+//        if let cell = tableView.cellForRow(at: indexPath) as? NewsCell {
+//            cell.newsText.numberOfLines = cell.newsText.calculateMaxLines()
+//        }
+//        tableView.endUpdates()
+//    }
+//}
+//
+//extension NewsTableViewController {
+//    func parseTableData(news: NewsPost, completion: @escaping (NewsPostViewModel) -> ()) {
+//        DispatchQueue.global().async {
+//            var authorName = ""
+//            var authorAvatar: URL
+//            if news.sourceID > 0 {
+//                authorName = self.users[news.sourceID]!.name
+//                authorAvatar = self.users[news.sourceID]!.avatarURL
+//            } else {
+//                authorName = self.groups[-news.sourceID]!.name
+//                authorAvatar = self.groups[-news.sourceID]!.avatarURL
+//            }
+//            
+//            let postViewModel = NewsPostViewModel(newsPost: news, authorName: authorName, avatarURL: authorAvatar)
+//            completion(postViewModel)
+//        }
+//    }
 }
---
+++ VKModel/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  MProject
+//
+//  Created by Danil G. on 04.04.2021.
+//
+
+import Foundation
