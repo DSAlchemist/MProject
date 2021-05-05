4f3b385a762053ad746f195cc79f73ddf7c57012
DSAlchemist <ccdefs@gmail.com>
2021-05-05 20:09:24 +0300

    Update 7.1

--- News/NewsFeedViewController.swift
+++ News/NewsFeedViewController.swift
@@ -11,10 +11,10 @@ import UIKit
 
 class NewsFeedTableViewController: UITableViewController {
 
-//    let networkManager = NetworkManager.shared
-//    private var newsPosts = [NewsPost]()
-//    private var users = [Int:User]()
-//    private var groups = [Int:Group]()
+    let networkManager = NetworkManager.shared
+    private var newsPosts = [NewsPost]()
+    private var users = [Int:User]()
+    private var groups = [Int:Group]()
 //
 //    override func viewDidLoad() {
 //        super.viewDidLoad()
@@ -44,37 +44,7 @@ class NewsFeedTableViewController: UITableViewController {
 //       let url: URL
 //        
 //        
-//        // Добавим вычисляемый параметр aspectRatio
-//          var aspectRatio: CGFloat { return CGFloat(height)/CGFloat(width) }
-//         
-//          init?(json: JSON) {
-//              guard let sizesArray = json["photo"]["sizes"].array,
-//                  let xSize = sizesArray.first(where: { $0["type"].stringValue == "x" }),
-//                  let url = URL(string: xSize["url"].stringValue) else { return nil }
-//             
-//              self.width = xSize["width"].intValue
-//              self.height = xSize["height"].intValue
-//              self.url = url
-//              let timeInterval = json["date"].doubleValue
-//              self.date = Date(timeIntervalSince1970: timeInterval)
-//              self.id = json["id"].intValue
-//          }
-//       }
-//    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//           switch indexPath.row {
-//           // Ячейки с фото у нас, например, имеют .row == 2
-//           case 2:
-//                   // Вычисляем высоту
-//                   let tableWidth = tableView.bounds.width
-//                   let news = self.news[indexPath.section]
-//                   let cellHeight = tableWidth * news.aspectRatio
-//                   return cellHeight
-//           default:
-//           // Для всех остальных ячеек оставляем автоматически определяемый размер
-//                   return UITableView.automaticDimension
-//              
-//           }
-//       }
+//
 
 //
 //    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -128,3 +98,36 @@ class NewsFeedTableViewController: UITableViewController {
 //        }
 //    }
 }
+
+
+// Добавим вычисляемый параметр aspectRatio
+//          var aspectRatio: CGFloat { return CGFloat(height)/CGFloat(width) }
+//
+//          init?(json: JSON) {
+//              guard let sizesArray = json["photo"]["sizes"].array,
+//                  let xSize = sizesArray.first(where: { $0["type"].stringValue == "x" }),
+//                  let url = URL(string: xSize["url"].stringValue) else { return nil }
+//
+//              self.width = xSize["width"].intValue
+//              self.height = xSize["height"].intValue
+//              self.url = url
+//              let timeInterval = json["date"].doubleValue
+//              self.date = Date(timeIntervalSince1970: timeInterval)
+//              self.id = json["id"].intValue
+//          }
+//       }
+//    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//           switch indexPath.row {
+//           // Ячейки с фото у нас, например, имеют .row == 2
+//           case 2:
+//                   // Вычисляем высоту
+//                   let tableWidth = tableView.bounds.width
+//                   let news = self.news[indexPath.section]
+//                   let cellHeight = tableWidth * news.aspectRatio
+//                   return cellHeight
+//           default:
+//           // Для всех остальных ячеек оставляем автоматически определяемый размер
+//                   return UITableView.automaticDimension
+//
+//           }
+//       }
---
+++ VKModel/NewsPost.swift
@@ -0,0 +1,8 @@
+//
+//  NewsPost.swift
+//  MProject
+//
+//  Created by Danil G. on 05.05.2021.
+//
+
+import Foundation
